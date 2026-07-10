d383d684928cd8a63d55c6a7e7e89225e435b372
marvai <arvai.misi@gmail.com>
2026-07-10 20:47:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -71,17 +71,51 @@ document.addEventListener('DOMContentLoaded', () => {
   setLanguage(savedLang);
 
   // 3. KAPCSOLATI ŰRLAP KEZELÉSE
-  if (contactForm) {
-    contactForm.addEventListener('submit', (e) => {
-      e.preventDefault();
-      const currentLang = htmlTag.getAttribute('data-lang');
-      if (currentLang === 'en') {
-        alert('Thank you for your message! I will get back to you soon.');
+  
+const form = document.getElementById("form");
+const result = document.getElementById("result");
+
+form.addEventListener("submit", function (e) {
+  const formData = new FormData(form);
+  e.preventDefault();
+  var object = {};
+  formData.forEach((value, key) => {
+    object[key] = value;
+  });
+  var json = JSON.stringify(object);
+  result.innerHTML = "Please wait...";
+
+  fetch("https://api.web3forms.com/submit", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: json
+  })
+    .then(async (response) => {
+      let json = await response.json();
+      if (response.status == 200) {
+        result.innerHTML = json.message;
+        result.classList.remove("text-gray-500");
+        result.classList.add("text-green-500");
       } else {
-        alert('Köszönöm az üzenetet! Hamarosan felveszem Önnel a kapcsolatot.');
+        console.log(response);
+        result.innerHTML = json.message;
+        result.classList.remove("text-gray-500");
+        result.classList.add("text-red-500");
       }
-      contactForm.reset();
+    })
+    .catch((error) => {
+      console.log(error);
+      result.innerHTML = "Something went wrong!";
+    })
+    .then(function () {
+      form.reset();
+      setTimeout(() => {
+        result.style.display = "none";
+      }, 5000);
     });
-  }
+});
 
 });
